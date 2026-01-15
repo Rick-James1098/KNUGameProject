77a2a71a67a1f733091b857f5a08b9dcb82beced
psuheon <tony356527@gmail.com>
2026-01-15 14:28:59 +0900

    소라고동 기믹 수정 및 난이도 추가

--- Assets/Scripts/Gimic/ConchGimic/ConchGameManager.cs
+++ Assets/Scripts/Gimic/ConchGimic/ConchGameManager.cs
@@ -6,28 +6,63 @@ public class ConchGameManager : MonoBehaviour
     public GameObject clickTarget; // 클릭할 원 프리팹 연
     public Vector2 minPosition;   // 생성 범위 최소 (좌측 하단)
     public Vector2 maxPosition;   // 생성 범위 최대 (우측 상단)
+
+    [Header("Difficulty Balance")]
+    public AnimationCurve difficultyCurve;
+
+    private float currentTime = 20f;       // 현재 남은 시간
+    private bool isPlaying = false;  // 게임 진행 중인지 여부
+    private float difficulty = 10f;
+    private float count = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        float randomRoll = Random.value;
+        difficulty = difficultyCurve.Evaluate(randomRoll);
+        Debug.Log(difficulty);
+        isPlaying = true;
         SpawnTarget();
+    }
+
+    void Update()
+    {
+        // 게임 중이 아니면 타이머 멈춤
+        if (!isPlaying) return;
+
+        // 2. 시간 감소 (카운트다운)
+        currentTime -= Time.deltaTime;
+
+        // 3. 시간이 다 됐는지 체크
+        if (currentTime <= 0)
+        {
+            EndGame();
+        }
     }
 
     public void SpawnTarget()
     {
+        if (!isPlaying) return;
         // 1. 랜덤 위치 계산
         float x = Random.Range(minPosition.x, maxPosition.x);
         float y = Random.Range(minPosition.y, maxPosition.y);
         Vector3 pos = new Vector3(x, y, 0);
-
         // 2. 드럼 생성
         GameObject tab = Instantiate(clickTarget, pos, Quaternion.identity);
 
         // 3. 드럼에게 매니저(나)를 알려줌
-        tab.GetComponent<TabController>().Setup(this);
+        tab.GetComponent<TabController>().Setup(this, difficulty);
     }
 
     public void OnClicked()
-    {
+    {   
+        count++;
+        Debug.Log(count);
         SpawnTarget();
     }
 
@@ -35,9 +70,11 @@ public class ConchGameManager : MonoBehaviour
     {
         SpawnTarget();
     }
-    // Update is called once per frame
-    void Update()
+
+    void EndGame()
     {
-        
+        isPlaying = false;
+        currentTime = 0;
     }
+
 }
--- Assets/Scripts/Gimic/ConchGimic/TabController.cs
+++ Assets/Scripts/Gimic/ConchGimic/TabController.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class TabController : MonoBehaviour
 {
+    private Animator animator;
     private ConchGameManager manager;
     private bool isClicked = false;
     public float lifeTime = 2.0f;
 
-    public void Setup(ConchGameManager gm)
+    public void Setup(ConchGameManager gm, float speedMultiplier)
     {
         manager = gm;
+        animator = GetComponent<Animator>();
         
+        float originalClipLength = 2.0f;
+        lifeTime = originalClipLength / speedMultiplier;
+
+        if (animator != null) animator.speed = speedMultiplier;
+
+
         // 1. 애니메이션 길이만큼 기다렸다가 사라지는 타이머 시작
         StartCoroutine(LifeCycleRoutine());
     }
